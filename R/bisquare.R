86775d717ca1c898b247cbb704023c76b8eedba6
richan <21110700112@m.fudan.edu.cn>
2024-03-06 04:57:37 +0800

    3.6 4:57 update, S-estimate

--- R/bisquare.R
+++ R/bisquare.R
@@ -1,24 +1,24 @@
 #' Title
 #'
 #' @param x
-#' @param k
+#' @param c
 #'
 #' @return
 #' @export
 #'
 #' @examples
-rho <- function(x, k) {
-  (1-(1-(x/k)^2)^3)*(abs(x)<=k) + 1*(abs(x)>k)
+rho <- function(x, c) {
+  (1-(1-(x/c)^2)^3)*(abs(x/c)<=1) + 1*(abs(x/c)>1)
 }
 
-psi <- function(x, k) {
-  x*(1-(x/k)^2)^2*(abs(x)<=k)
+psi <- function(x, c) {
+  x*(1-(x/c)^2)^2*(abs(x/c)<=1)
 }
 
-wgt_m <- function(x, k) {
-  psi(x, k)/x
+wgt_m <- function(x, c) {
+  psi(x, c)/(x^2+1e-32)*(x!=0) + 1*(x==0)
 }
 
-wgt_s <- function(x, k) {
-  rho(x, k)/x^2
+wgt_s <- function(x, c) {
+  rho(x, c)/(x^2+1e-32)*(x!=0) + 6/c^2*(x==0)
 }
